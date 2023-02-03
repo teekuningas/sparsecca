405bb92f4ba098c96bd7a2523e66a723df2f9f9b
Michaela Mueller <mumichae@in.tum.de>
2023-02-03 15:15:39 +0100

    include spearman rank correlation comparison of p-val and z-stat

--- tests/test_multicca_permute.py
+++ tests/test_multicca_permute.py
@@ -2,6 +2,7 @@ from pprint import pprint
 import numpy as np
 import pandas as pd
 import sparsecca
+from scipy.stats import spearmanr
 
 
 def test_multicca_permute():
@@ -19,25 +20,33 @@ def test_compare_multicca_permute_to_r():
     ]
 
     # R PMA::MultiCCA.permute output for same data
-    cors = np.array(
-        [0.4986115, 0.4986115, 0.4986115, 0.4986115, 0.4986115,
-         0.49482022, 0.48099667, 0.48651162, 0.48216657, 0.47484963]
-    )
+    cors = np.array([
+        0.4986115, 0.4986115, 0.4986115, 0.4986115, 0.4986115, 0.4948202, 0.4809967, 0.4865116, 0.4821666, 0.4748496
+    ])
     penalties = np.array([
-        [1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.26710519,
-            1.44102159, 1.61493798, 1.78885438],
-        [1.1, 1.1, 1.1, 1.1, 1.1, 1.19752832, 1.38804419,
-            1.57856006, 1.76907593, 1.95959179]
+        [1.1, 1.1, 1.1, 1.1, 1.1, 1.100000, 1.267105, 1.441022, 1.614938, 1.788854],
+        [1.1, 1.1, 1.1, 1.1, 1.1, 1.197528, 1.388044, 1.578560, 1.769076, 1.959592]
+    ])
+    pvals = np.array([
+        0.242, 0.242, 0.242, 0.242, 0.242, 0.293, 0.448, 0.511, 0.556, 0.599
+    ])
+    zstat = np.array([
+        0.467956119157931, 0.467956119157931, 0.467956119157931, 0.467956119157931, 0.467956119157931, 0.365482477335275, 0.0579137832284457, -
+        0.0553037219023547, -0.120807884563517, -0.190823297491557
     ])
 
     res = sparsecca.multicca_permute(
         datasets,
-        nperms=10,
-        niter=25,
+        nperms=1000,
+        niter=100,
         standardize=True,
     )
 
     pprint(res)
-    rtol=1e-9
+    rtol = 1e-6
     assert np.allclose(cors, res['cors'], rtol=rtol)
     assert np.allclose(penalties, res['penalties'], rtol=rtol)
+
+    # check statistics match somewhat
+    assert spearmanr(pvals, res['pvals'])[0] == 1
+    assert spearmanr(zstat, res['zstat'])[0] == 1
